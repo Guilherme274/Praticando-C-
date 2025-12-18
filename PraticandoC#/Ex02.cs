9c61e365edff6bfe961d33c1e94d25baa658cb7f
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2025-12-17 21:37:42 -0300

    Resolvendo segundo exercício

---
+++ PraticandoC#/Ex02.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+
+namespace PraticandoC_
+{
+    public class Ex02
+    {
+        class RegistroCompras
+        {
+            public DateOnly data { get; set; }
+            public DateTime hora { get; set; }
+            public string produto { get; set; }   
+            List<RegistroCompras> registros = new List<RegistroCompras>();
+            public RegistroCompras(DateOnly data, DateTime hora, string produto)
+            {
+                this.data = data;
+                this.hora = hora;
+                this.produto = produto;
+            }
+
+            public void AdicionarCompra(RegistroCompras registro)
+            {   
+                Console.WriteLine("Registro Adicionado");
+                registros.Add(registro);
+            }
+            
+            public void ListarCompras()
+            {
+                Console.WriteLine("Listagem de Registros");
+                foreach(RegistroCompras registro in registros)
+                {
+                    Console.WriteLine($"\n   Data: {registro.data} \n   Hora: {registro.hora} \n   Produto: {registro.produto}");
+                }
+            }
+        }
+
+        public static void Executar()
+        {
+            // Criando registros
+            RegistroCompras registro1 = new RegistroCompras(DateOnly.MaxValue, DateTime.Now, "Tênis");
+            RegistroCompras registro2 = new RegistroCompras(DateOnly.MaxValue, DateTime.Now, "Mochila");
+            registro1.AdicionarCompra(registro1);
+            registro1.AdicionarCompra(registro2);
+
+            registro1.ListarCompras();
+
+        }
+    }
+}
