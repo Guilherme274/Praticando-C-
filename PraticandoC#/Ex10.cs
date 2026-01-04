350ee9f5e1c490b38655e971075364e1ae0fc264
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-01-03 22:34:10 -0300

    Resolução Exercicio 10 (necessidade de testes)

--- PraticandoC#/Ex10.cs
+++ PraticandoC#/Ex10.cs
@@ -2,5 +2,39 @@
 
 public class Ex10
 {
+    public class Animal
+    {
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+        public int Idade { get; set; }
 
+        public Animal(string nome, string especie, int idade)
+        {
+            Nome = nome; 
+            Especie = especie;
+            Idade = idade;
+        }
+
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine($"{Nome}, da espécie: {Especie}, com {Idade} ano(s). Está emitindo som");
+        }
+    }
+
+    public class Chimpanze : Animal
+    {
+        public Chimpanze(string nome, string especie, int idade) : base(nome, especie, idade)
+        {
+        }
+
+        public override void EmitirSom()
+        {
+            Console.WriteLine($"Chimpanzé, da espécie: {Especie}, com {Idade} ano(s). Está emitindo som");
+        }
+    }
+
+    public static void Executar()
+    {
+        
+    }
 }
