c2c34d8601b2d7f437a254ac3b7c4f701123c055
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2026-01-03 22:24:29 -0300

    Resolução Exercício 09 (necessidade de testes)

--- PraticandoC#/Ex09.cs
+++ PraticandoC#/Ex09.cs
@@ -2,5 +2,42 @@
 
 public class Ex09
 {
+    public class Livro
+    {
+        private string _titulo;
+        public string Titulo {
+                get
+                {
+                    return _titulo;
+                } 
+                set
+                {
+                    if(value == null)
+                    {
+                        throw new Exception("Os valores não podem ser vazios");
+                    }
+                } 
+            }
+        private string _autor;
+        public string Autor {
+                get
+                {
+                    return _titulo;
+                } 
+                set
+                {
+                    if(value == null)
+                    {
+                        throw new Exception("Os valores não podem ser vazios");
+                    }
+                }
+        }
+    }
 
+    public static void Executar()
+    {
+        Livro livro = new Livro();
+
+        Console.WriteLine(livro.Autor);
+    }
 }
